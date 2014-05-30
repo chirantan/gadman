63a977fd1dc15fa93dc42ba4c3083e62727d8d6b
Chirantan <chirantan.rajhans@gmail.com>
2014-05-30 15:06:09 +0200

    Implement Gadget.search and write first test case

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -2,4 +2,8 @@ class Gadget < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :name, presence: true
+
+  def self.search(name)
+    where("name like '%#{name}%'")
+  end
 end
--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -17,7 +17,12 @@ describe Gadget, :type => :model do
       @thinkpad   = user.gadgets.create :name => "Thinkpad"
     end
 
-    it "should find cell_phone and iphone whensearch for 'phone'"
+    it "should find cell_phone and iphone whensearch for 'phone'" do
+      results = Gadget.search("phone")
+      expect(results).to include @cell_phone
+      expect(results).to include @iphone
+    end
+
     it "should find iphone and ipad for 'IP'"
     it "should find ipad and thinkpad for 'pad'"
     it "should not find anything when searched for 'neo' "
